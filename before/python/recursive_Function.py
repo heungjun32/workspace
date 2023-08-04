3a05edc638795738208c3915c6bf35256b2ebab6
Im HeungJun <heungjun325@gmail.com>
2023-08-04 14:59:46 +0900

    second commit

--- before/python/recursive_Function.py
+++
@@ -1,8 +0,0 @@
-
-j = int(input("Input Number for Factorial Calculation:"))
-def multiplication(j):
-    result = 1
-    for num in range(1, j+1):
-         result *= num
-    return result
-print (multiplication(j))    
--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 print(3+0)
 print(2+1)
+print(3)
